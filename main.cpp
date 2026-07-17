db13d11fcad106f3ac86cd474daa467a0d983185
Thorolund <andrey.sobol.1510@gmail.com>
2026-07-17 16:23:29 +0300

    add sdl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-	std::cout << "Cout!";
+int main(int argc, char* argv[]) {
+	if (SDL_Init(SDL_INIT_EVERYTHING)) {
+		std::cout << "Bad:(";
+		return 1;
+	}
+	else {
+		std::cout << "Supper!";
+		SDL_Quit();
+	}
 	return 0;
 }
